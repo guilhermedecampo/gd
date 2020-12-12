a00ac78cf08be26919db0add164954bb230dfd16
Guilherme Decampo <guilherme.decampo@gmail.com>
2020-12-12 08:19:09 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Guilherme Decampo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Guilherme Decampo" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Hello! 
         </p>
       </main>
 
